f8e7f74ac9bf8694ad007007d4d67f63d3bb7de2
Randi Mikkelsen Bech <au615785@uni.au.dk>
2021-11-28 20:51:22 +0100

    Add workouts to client

--- src/Client/ClientDetail.js
+++ src/Client/ClientDetail.js
@@ -1,13 +1,93 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { useLocation } from "react-router";
+import { getWorkoutPrograms, addWorkoutProgramForClient } from '../Helpers/WorkoutProgramsApi';
+import { makeStyles } from '@mui/styles';
 
-function ClientDetail(){
+const useStyles = makeStyles({
+    leftColoumn: {
+        float: 'left',
+        width: '50%',
+        height: '100%'
+    },
+    rightColoumn: {
+        float: 'right',
+        width: '50%',
+        height: '100%'
+    },
+    programs: {
+        padding: '0 20px'
+    },
+    programItem: {
+        textAlign: 'left',
+        margin: '8px 0',
+        cursor: 'pointer',
+        backgroundColor: 'lightgray'
+    },
+    line: {
+        height: '0.5px',
+        backgroundColor: 'black',
+        width: '100%'
+    },
+    formItem: {
+        textAlign: 'left',
+        margin: '4px 0'
+    },
+    label: {
+        marginRight: '8px'
+    }
+})
+
+function ClientDetail() {
     const location = useLocation();
+    const classes = useStyles();
     const client = location.state;
-    console.log('client', location.state )
+    const [workoutPrograms, setWorkoutPrograms] = useState([]);
+    const [newProgramName, setNewProgramName] = useState('');
+    const [newProgramDescription, setNewProgramDescription] = useState('');
 
+    useEffect(() => {
+        async function getPrograms() {
+            const programs = await getWorkoutPrograms();
+            setWorkoutPrograms(programs);
+            console.log('programs', programs);
+        }
+        getPrograms();
+    }, []); 
 
+    function handleProgramName(event){
+        setNewProgramName(event.target.value);
+    }
+
+    function handleProgramDescription(event){
+        setNewProgramDescription(event.target.value);
+    }
+
+    async function handleAddWorkoutProgram(event){
+        event.preventDefault();
+
+        //temp:
+        const jwtToken = localStorage.getItem('jwtToken');
+        const tokenArray = jwtToken.split(".");
+        const payload = window.atob(tokenArray[1].split(".")[0]);
+        const payloadObj = JSON.parse(payload);
+        console.log('payload', payloadObj);
+        console.log('payload', payloadObj.UserId);
+
+        const workoutProgramRequest = {
+            workoutProgramId: 0,
+            name: newProgramName,
+            description: newProgramDescription,
+            exercises: [],
+            personalTrainerId: payloadObj.UserId,
+            clientId: client.userId
+        }
+        //fejler CORS fejl
+        await addWorkoutProgramForClient(workoutProgramRequest);
+
+        const newPrograms = workoutPrograms;
+        newPrograms.push(workoutProgramRequest);
+        setWorkoutPrograms(newPrograms);
+    }
 
     return (
         <div>
@@ -18,9 +98,33 @@ function ClientDetail(){
             <p>
                 Email: {client.email}
             </p>
-            <div>
-                
-            </div>
+                <div className={classes.leftColoumn}>
+                    <h4>{client.firstName}'s workout programs:</h4>
+                    <div className={classes.programs}>
+                        {workoutPrograms.map((program) => (
+                            <div key={program.workoutProgramId} className={classes.programItem}>
+                                <div>{program.name}</div>
+                                <div>{program.description}</div>
+                                <div className={classes.line}></div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className={classes.rightColoumn}>
+                    <h4>Add a workout program to {client.firstName}:</h4>
+                    <form onSubmit={handleAddWorkoutProgram}>
+                        <div className={classes.formItem}>
+                            <label className={classes.label}>Name:</label>
+                            <input type="text" value={newProgramName} onChange={handleProgramName}/>
+                        </div>
+                        <div className={classes.formItem}>
+                            <label className={classes.label}>Description:</label>
+                            <input type="text" value={newProgramDescription} onChange={handleProgramDescription}/>
+                        </div>
+                        <input type="submit" value="Submit" />
+                    </form>
+                    
+                </div>
         </div>
     )
 }

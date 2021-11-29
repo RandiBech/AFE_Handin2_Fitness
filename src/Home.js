35806d0612767a88dee9ef4fd5eff9b4c2f37177
Quynh Trang Mac <201810269@post.au.dk>
2021-11-29 11:03:12 +0100

    manager fixed

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom";
 import Login from "./Login/Login";
-import { Roles } from "./Helpers/Roles";
+import PersonalTrainerAdd from "./PersonalTrainer/PersonalTrainerAdd";
 import "./Home.css";
-import ClientList from "./Client/ClientList";
-import WorkoutProgramList from "./WorkoutProgram/WorkoutProgramList";
+
 function Home() {
   var role = localStorage.Roles; //localStorage.PersonalTrainer; //TODO: Husk at rette dette til.
-
+  var role = "Manager";
+  console.log(role);
+  <PersonalTrainerAdd />;
   if (role != null) {
     return <h1>Welcome</h1>;
+  } else if ((role = "Manager")) {
+    return <PersonalTrainerAdd />;
   } else
     return (
       <div>
@@ -16,42 +18,5 @@ function Home() {
         <Login />
       </div>
     );
-  // if (role === Roles.Manager) {
-  //   return (
-  //     <div>
-  //       <h1>Welcome {role}!</h1>
-  //       {/* <CreatePersonalTrainer /> */}
-  //     </div>
-  //   );
-  // } else if (role === Roles.PersonalTrainer) {
-  //   return (
-  //     <div>
-  //       <h1>Welcome: Personal Trainer!</h1>
-  //       {/* <CreateClients />; */}
-  //       <div class="container">
-  //         <div id="split_left" class="parent">
-  //           <WorkoutProgramList />
-  //         </div>
-  //         <div id="split_right" class="parent">
-  //           <ClientList />
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // } else if (role === Roles.Client) {
-  //   return (
-  //     <div>
-  //       <h1>Welcome: Client!</h1>
-  //       <WorkoutProgramList />
-  //     </div>
-  //   );
-  // } else {
-  //   return (
-  //     <div>
-  //       <h1>Welcome stranger! You need to login</h1>
-  //       <Link to="login">Login</Link>
-  //     </div>
-  //   );
-  // }
 }
 export default Home;
--- src/PersonalTrainer/PersonalTrainerAdd.js
+++ src/PersonalTrainer/PersonalTrainerAdd.js
@@ -2,23 +2,13 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react/cjs/react.development";
 import { useForm } from "react-hook-form";
-import "./ClientAdd.css";
-// Inspo from demo forms-demos: MultipleInput.js
-// Inspo from https://www.youtube.com/watch?v=IkMND33x0qQ & https://www.youtube.com/watch?v=EcRFYF4B3IQ
-export function ClientAdd() {
+
+function PersonalTrainerAdd() {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  //Temp: TODO - Delete
-  const jwtToken = localStorage.getItem("jwtToken");
-  const tokenArray = jwtToken.split(".");
-  const payload = window.atob(tokenArray[1].split(".")[0]);
-  const payloadObj = JSON.parse(payload);
-
-  console.log("payload", payloadObj);
-  console.log("payload", payloadObj.UserId);
   const handleSumitClient = async () => {
     fetch("https://afe2021fitness.azurewebsites.net/api/Users", {
       method: "POST",
@@ -32,8 +22,7 @@ export function ClientAdd() {
         lastName: lastname,
         email: email,
         password: password,
-        personalTrainerId: payloadObj.UserId,
-        accountType: "Client",
+        accountType: "PersonalTrainer",
       }),
     }).then((response) => {
       console.log(response);
@@ -42,7 +31,7 @@ export function ClientAdd() {
 
   return (
     <div className="add-client">
-      <h2>Add Client</h2>
+      <h2>Add Personal Trainer</h2>
       <form onSubmit={handleSumitClient}>
         <label>
           First name:
@@ -100,3 +89,5 @@ export function ClientAdd() {
     </div>
   );
 }
+
+export default PersonalTrainerAdd;

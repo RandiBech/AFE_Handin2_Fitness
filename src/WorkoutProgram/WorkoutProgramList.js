d14572f9e9fd3b22583e7b9c3b8b1a8ffa8ada39
Mads Bo Mikkelsen <au615041@uni.au.dk>
2021-11-30 11:25:19 +0100

    fixed conditional stament in workoutprogramList

--- src/WorkoutProgram/WorkoutProgramList.js
+++ src/WorkoutProgram/WorkoutProgramList.js
@@ -1,45 +1,44 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Roles } from "../Helpers/Roles";
-import { TrainerTabel } from "./TrainerTabel";
-import { ClientTabel } from "./ClientTabel";
-import { Outlet } from "react-router";
-import { useAuthContext } from "../Context/auth-context";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Roles } from '../Helpers/Roles';
+import { TrainerTabel } from './TrainerTabel';
+import { ClientTabel } from './ClientTabel';
+import { Outlet } from 'react-router';
+import { useAuthContext } from '../Context/auth-context';
 
 export default function WorkoutProgramList(props) {
-  const [state, setState] = useState({ workoutPrograms: [] });
-  const { tokenPayload, jwtToken } = useAuthContext();
+	const [state, setState] = useState({ workoutPrograms: [] });
+	const { tokenPayload, jwtToken } = useAuthContext();
 
-  const config = {
-    headers: { Authorization: `Bearer ${jwtToken}` },
-  };
+	const config = {
+		headers: { Authorization: `Bearer ${jwtToken}` },
+	};
 
-  useEffect(() => {
-    async function getWorkoutPrograms() {
-      const baseUrl =
-        "https://afe2021fitness.azurewebsites.net/api/workoutPrograms";
-      var rolePath = "";
-      if (tokenPayload.Role === Roles.PersonalTrainer) {
-        rolePath = "/trainer";
-      } else if (tokenPayload.Role === Roles.Client) {
-        rolePath = `/client/${tokenPayload.UserId}`;
-      }
+	useEffect(() => {
+		async function getWorkoutPrograms() {
+			const baseUrl = 'https://afe2021fitness.azurewebsites.net/api/workoutPrograms';
+			var rolePath = '';
+			if (tokenPayload.Role === Roles.PersonalTrainer) {
+				rolePath = '/trainer';
+			} else if (tokenPayload.Role === Roles.Client) {
+				rolePath = `/client/${tokenPayload.UserId}`;
+			}
 
-      const res = await axios.get(`${baseUrl}${rolePath}`, config);
-      setState({ workoutPrograms: res.data });
-    }
-    getWorkoutPrograms();
-  }, []);
+			const res = await axios.get(`${baseUrl}${rolePath}`, config);
+			setState({ workoutPrograms: res.data });
+		}
+		getWorkoutPrograms();
+	}, []);
 
-  return (
-    <div>
-      <h2>Workouts</h2>
-      {jwtToken.Role === Roles.PersonalTrainer ? (
-        <TrainerTabel workoutPrograms={state.workoutPrograms} />
-      ) : (
-        <ClientTabel workoutPrograms={state.workoutPrograms} />
-      )}
-      <Outlet />
-    </div>
-  );
+	return (
+		<div>
+			<h2>Workouts</h2>
+			{tokenPayload.Role === Roles.PersonalTrainer ? (
+				<TrainerTabel workoutPrograms={state.workoutPrograms} />
+			) : (
+				<ClientTabel workoutPrograms={state.workoutPrograms} />
+			)}
+			<Outlet />
+		</div>
+	);
 }
